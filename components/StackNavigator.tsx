4fca90b2c6c85d3fffc8a0a859e40d2a8627a18b
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-10-30 00:12:24 -0500

    Added idolatry to tab bar

--- components/StackNavigator.tsx
+++ components/StackNavigator.tsx
@@ -9,6 +9,9 @@ import Credentials from '../screens/credentials/CredentialsScreen';
 import CredentialModification from '../screens/credentials/CredentialModificationScreen';
 import * as Haptics from 'expo-haptics';
 import Profile from '../screens/ProfileScreen';
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
@@ -16,9 +19,33 @@ const Tabs = createBottomTabNavigator();
 function TabNavigator() { //Contains all pages that have bottom tab navigator
     return (
         <Tabs.Navigator initialRouteName = "Home" screenOptions={{tabBarStyle: {backgroundColor: "black"}}}>
-            <Tabs.Screen name="Credentials" component={Credentials} listeners={{tabPress: () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}/>
-            <Tabs.Screen name="Profile" component={Profile} listeners={{tabPress: () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}/>
-            <Tabs.Screen name="Settings" component={Settings} listeners={{tabPress: () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}/>
+            <Tabs.Screen 
+              name="Credentials"
+              component={Credentials} 
+              listeners={{tabPress: () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}
+              options={{
+                tabBarLabel: 'Credentials',
+                tabBarIcon: () => <Entypo name="documents" size={24} color="white" />
+              }}
+            />
+            <Tabs.Screen
+              name="Profile"
+              component={Profile}
+              listeners={{tabPress: () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}
+              options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: () => <Ionicons name="person" size={24} color="white" />
+              }}
+            />
+            <Tabs.Screen
+              name="Settings"
+              component={Settings}
+              listeners={{tabPress: () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}
+              options={{
+                tabBarLabel: 'Settings',
+                tabBarIcon: () => <FontAwesome name="gear" size={24} color="white" />
+              }}
+            />
         </Tabs.Navigator>
     );
 }
