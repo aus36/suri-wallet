66abf3450025652067738574b85c6d8397891494
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-10-30 00:20:49 -0500

    Started on credentials and settings screens

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,11 +1,69 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import useAuth from '../hooks/useAuth';
+import { useLayoutEffect } from 'react';
 
 const Settings = () => {
+
+    const navigation = useNavigation(); // enable navigation
+    const { user } = useAuth(); // enable auth functionality
+
+    //disable header/gestures
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+            gestureEnabled: false,
+        });
+    }, [navigation]);
+
     return (
-        <View>
-            <Text>Settings Screen</Text>
+        <View style = {styles.container}>
+            <Text style = {styles.headerText}>Settings Screen</Text>
         </View>
     );
 };
 
 export default Settings;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerText: {
+        fontSize: 24,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        marginBottom: 20
+    },
+    bodyText: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    actionButton: {
+        backgroundColor: '#1E1E1E',
+        borderRadius: 10,
+        padding: 10,
+        margin: 10,
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logoutButton: {
+        backgroundColor: '#1E1E1E',
+        borderRadius: 10,
+        padding: 10,
+        margin: 10,
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+});
--- screens/credentials/CredentialsScreen.tsx
+++ screens/credentials/CredentialsScreen.tsx
@@ -1,11 +1,78 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import useAuth from '../../hooks/useAuth';
+import { useLayoutEffect } from 'react';
 
 const Credentials = () => {
+
+    const navigation = useNavigation(); // enable navigation
+    const { user } = useAuth(); // enable auth functionality
+
+    //disable header/gestures
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+            gestureEnabled: false,
+        });
+    }, [navigation]);
+
     return (
-        <View>
-            <Text>Credentials Screen</Text>
+        <View style = {styles.container}>
+            <Text style = {styles.headerText}>Credentials Screen</Text>
         </View>
     );
 };
 
 export default Credentials;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontSize: 24,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    bodyText: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    actionButton: {
+        backgroundColor: '#1E1E1E',
+        borderRadius: 10,
+        padding: 10,
+        margin: 10,
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logoutButton: {
+        backgroundColor: '#1E1E1E',
+        borderRadius: 10,
+        padding: 10,
+        margin: 10,
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    profileLogo: {
+        marginTop: 20,
+        marginBottom: 20,
+    },
+});
