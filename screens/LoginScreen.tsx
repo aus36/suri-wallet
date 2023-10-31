b53f6e1f6634bce6fe7f7db133b82f208232f717
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-10-31 13:55:59 -0500

    Linear gradient moment

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/core';
 import useAuth from '../hooks/useAuth';
 import React, { useState, useMemo } from 'react';
@@ -7,7 +7,7 @@ import React, { useState, useMemo } from 'react';
 const Login = () => {
 
   const navigation = useNavigation(); // enable navigation
-  const { user, login, register  } = useAuth(); // enable user functionality
+  const { user, login, register } = useAuth(); // enable user functionality
 
   const [pin, setPin] = useState('');
   const [displayPin, setDisplayPin] = useState('');
@@ -19,7 +19,7 @@ const Login = () => {
     });
   }, [navigation]);
 
-  useMemo(() => {
+  useMemo(() => { // useMemo to make sure only the last character of the pin is visible
     if (!pin || pin.length === 0){
       setDisplayPin('_');
     }
@@ -52,59 +52,61 @@ const Login = () => {
     <View style={styles.container}>
       {/* Logo */}
       <Image source={require('../assets/SURI-logo.png')} style={styles.logo}/>
-      {/* Pin display */}
-      <View>
-        <Text style = {styles.displayPin}>{displayPin}</Text>
-      </View>
-      {/* Pinpad */}
-      <View style = {styles.pinPad}>
+      <LinearGradient start = {{x: 0, y: 0}} colors={["#4C4C4C", "#111111"]} style = {styles.gradientContainer}>
+        {/* Pin display */}
+        <View>
+          <Text style = {styles.displayPin}>{displayPin}</Text>
+        </View>
+        {/* Pinpad */}
+        <View style = {styles.pinPad}>
+          <View style={styles.pinRow}>
+            <TouchableOpacity onPress={() => handlePress("1")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("2")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("3")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>3</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.pinRow}>
+            <TouchableOpacity onPress={() => handlePress("4")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("5")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("6")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>6</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.pinRow}>
+            <TouchableOpacity onPress={() => handlePress("7")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("8")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>8</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("9")} style={styles.pinButton}>
+              <Text style = {styles.pinButtonText}>9</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* Buttons */}
         <View style={styles.pinRow}>
-          <TouchableOpacity onPress={() => handlePress("1")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>1</Text>
+          <TouchableOpacity // @ts-ignore 
+            onPress={() => {handleLogin();}}
+            style = {styles.loginButton}>
+            <Text style = {styles.actionButtonText}>Login</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => handlePress("2")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => handlePress("3")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>3</Text>
+          <TouchableOpacity
+            onPress={() => setPin(pin.slice(0, -1))}
+            style = {styles.backButton}>
+            <Text style = {styles.actionButtonText}>Delete</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.pinRow}>
-          <TouchableOpacity onPress={() => handlePress("4")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>4</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => handlePress("5")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>5</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => handlePress("6")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>6</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.pinRow}>
-          <TouchableOpacity onPress={() => handlePress("7")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>7</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => handlePress("8")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>8</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => handlePress("9")} style={styles.pinButton}>
-            <Text style = {styles.pinButtonText}>9</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      {/* Buttons */}
-      <View style={styles.pinRow}>
-        <TouchableOpacity // @ts-ignore 
-          onPress={() => {handleLogin();}}
-          style = {styles.loginButton}>
-          <Text style = {styles.actionButtonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => setPin(pin.slice(0, -1))}
-          style = {styles.backButton}>
-          <Text style = {styles.actionButtonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
+      </LinearGradient>
       <TouchableOpacity // @ts-ignore 
         onPress={() => navigation.navigate('Register')}
         style = {styles.registerButton}>
@@ -122,6 +124,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  gradientContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    padding: 15,
   },
   logo: {
     height: 200,
